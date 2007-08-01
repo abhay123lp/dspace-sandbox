23baccc2276db7652ba97934f93fa6f32e0d0222
grahamtriggs <grahamtriggs@2b3ed498-6d30-0410-9511-f9d6487c6e61>
2007-08-01 15:59:01 +0000

    First attempt at integrating the configurable browse patch

--- branches/browse/dspace-api/src/main/java/org/dspace/browse/CrossLinks.java
+++ branches/browse/dspace-api/src/main/java/org/dspace/browse/CrossLinks.java
@@ -1,11 +1,11 @@
 /*
- * InitializeBrowse.java
+ * CrossLinks.java
  *
- * Version: $Revision$
+ * Version: $Revision: 1.9 $
  *
- * Date: $Date$
+ * Date: $Date: 2005/04/20 14:23:12 $
  *
- * Copyright (c) 2002-2005, Hewlett-Packard Company and Massachusetts
+ * Copyright (c) 2002-2007, Hewlett-Packard Company and Massachusetts
  * Institute of Technology.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -39,58 +39,70 @@
  */
 package org.dspace.browse;
 
-import java.sql.SQLException;
+import java.util.HashMap;
 
-import org.dspace.core.Context;
+import org.dspace.core.ConfigurationManager;
 
 /**
- * Command-line tool to create Browse indexes.
+ * Class to represent the configuration of the cross-linking between browse
+ * pages (for example, between the author name in one full listing to the
+ * author's list of publications).
  * 
- * @author Peter Breton
- * @version $Revision$
+ * @author Richard Jones
+ *
  */
-public class InitializeBrowse
+public class CrossLinks
 {
-    /** Private Constructor */
-    private InitializeBrowse()
-    {
-    }
+	/** a map of the desired links */
+	private HashMap links = new HashMap();
+	
+	/**
+	 * Construct a new object which will obtain the configuration for itself
+	 * 
+	 * @throws BrowseException
+	 */
+	public CrossLinks()
+		throws BrowseException
+	{
+		int i = 1;
+		while (true)
+		{
+			String field = "webui.browse.link." + i;
+			String config = ConfigurationManager.getProperty(field);
+			if (config == null)
+			{
+				break;
+			}
+			
+			String[] parts = config.split(":");
+			if (parts.length != 2)
+			{
+				throw new BrowseException("Invalid configuration for " + field + ": " + config);
+			}
+			links.put(parts[1], parts[0]);
+			i++;
+		}
+	}
 
-    /**
-     * Creates Browse indexes, destroying the old ones.
-     * 
-     * @param argv
-     *            Command-line arguments
-     */
-    public static void main(String[] argv)
-    {
-        Context context = null;
-        int status = 0;
-
-        try
-        {
-            System.out.print("Indexing all Items in DSpace....");
-
-            context = new Context();
-            Browse.indexAll(context);
-            context.complete();
-
-            System.out.println(" ... Done");
-        }
-        catch (SQLException sqle)
-        {
-            status = 1;
-            if (context != null)
-            {
-                context.abort();
-            }
-
-            System.err.println("Error: Browse index NOT created");
-            sqle.printStackTrace();
-        }
-        finally
-        {
-            System.exit(status);
-        }
-    }
+	/**
+	 * Is there a link for the given canonical form of metadata (i.e. schema.element.qualifier)
+	 * 
+	 * @param metadata	the metadata to check for a link on
+	 * @return
+	 */
+	public boolean hasLink(String metadata)
+	{
+		return links.containsKey(metadata);
+	}
+	
+	/**
+	 * get the type of link that the bit of metadata has
+	 * 
+	 * @param metadata	the metadata to get the link type for
+	 * @return
+	 */
+	public String getLinkType(String metadata)
+	{
+		return (String) links.get(metadata);
+	}
 }
