390c3e593340ac546d07d4bdee49ab570a5d47d9
stuart.lewis <stuart.lewis@2b3ed498-6d30-0410-9511-f9d6487c6e61>
2007-08-07 21:20:56 +0000

    Added Exceptions

---
+++ branches/sword/dspace-sword/src/main/java/org/purl/sword/SWORDException.java
@@ -0,0 +1,34 @@
+package org.purl.sword;
+
+/**
+ * Represents a generic SWORD exception. If this thrown by a repository,
+ * it would result in a HTTP 500 message being returned to the user.
+ * 
+ * @author Stuart Lewis
+ * @author Neil Taylor
+ */
+public class SWORDException extends Exception 
+{
+   /**
+    * Create a new instance and store the specified message and source data. 
+    * 
+    * @param message The message for the exception. 
+    * @param source  The original exception that lead to this exception. This
+    *                can be <code>null</code>. 
+    */
+   public SWORDException(String message, Exception source)
+   {  
+      super(message, source);  
+   }
+   
+   /**
+    * Create a new instance and store the specified message. 
+    * 
+    * @param message The message for the exception. 
+    */
+   public SWORDException(String message)
+   {
+      super(message);
+   }
+   
+}
