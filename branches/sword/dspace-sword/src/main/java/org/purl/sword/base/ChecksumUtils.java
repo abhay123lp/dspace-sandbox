63fa537c5af68e30a5c0d4d5da43a25a5a516164
stuart.lewis <stuart.lewis@2b3ed498-6d30-0410-9511-f9d6487c6e61>
2007-08-14 21:07:23 +0000

    Added method to take byte array

--- branches/sword/dspace-sword/src/main/java/org/purl/sword/base/ChecksumUtils.java
+++ branches/sword/dspace-sword/src/main/java/org/purl/sword/base/ChecksumUtils.java
@@ -90,7 +90,55 @@ public class ChecksumUtils
 	      return md5; 
 	   }
    
-   
+   /**
+    * Generate an MD5 hash for the file that is specified in the 
+    * filepath. The hash is returned as a String representation. 
+    * 
+    * @param bytes The byte array to checksum. 
+    * @return A string hash of the file. 
+    * @throws NoSuchAlgorithmException If the MD5 algorithm is 
+    * not supported by the installed virtual machine. 
+    * 
+    * @throws IOException If there is an error accessing the file. 
+    */
+   public static String generateMD5(byte[] bytes)
+   throws NoSuchAlgorithmException, IOException 
+   {
+      String md5 = null; 
+      
+      try
+      {
+    	  MessageDigest md = MessageDigest.getInstance("MD5");
+    	  md.reset();
+         
+	      md.update(bytes);
+	         
+	
+	         byte[] md5Digest = md.digest();
+	
+	         StringBuffer buffer = new StringBuffer(); 
+	         for( byte b : md5Digest )
+	         {
+	            // 0xFF is used to handle the issue of negative numbers in the bytes
+	            String hex = Integer.toHexString(b & 0xFF);
+	            if( hex.length() == 1 )
+	            {
+	               buffer.append("0");
+	            }
+	            buffer.append(hex);
+	         }
+	
+	         md5 = buffer.toString();
+	      }
+	      catch(NoSuchAlgorithmException ex )
+	      {
+	          InfoLogger.getLogger().writeInfo("Error accessing string");
+	          throw ex; // rethrow 
+	      }
+	      
+	      return md5; 
+	   }
+      
    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 	   System.out.println(ChecksumUtils.generateMD5(args[0]));
    }
