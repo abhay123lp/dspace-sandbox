b78d903e7f28a18932ed10005c3aeda9e86d40c6
stuart.lewis <stuart.lewis@2b3ed498-6d30-0410-9511-f9d6487c6e61>
2007-08-18 20:22:47 +0000

    Added facility to hold error code

--- branches/sword/dspace-sword/src/main/java/org/purl/sword/base/SWORDException.java
+++ branches/sword/dspace-sword/src/main/java/org/purl/sword/base/SWORDException.java
@@ -9,26 +9,50 @@ package org.purl.sword.base;
  */
 public class SWORDException extends Exception 
 {
-   /**
-    * Create a new instance and store the specified message and source data. 
-    * 
-    * @param message The message for the exception. 
-    * @param source  The original exception that lead to this exception. This
-    *                can be <code>null</code>. 
-    */
-   public SWORDException(String message, Exception source)
-   {  
-      super(message, source);  
-   }
-   
-   /**
-    * Create a new instance and store the specified message. 
-    * 
-    * @param message The message for the exception. 
-    */
-   public SWORDException(String message)
-   {
-      super(message);
-   }
-   
+	private String errorCode;
+	
+	/**
+     * Create a new instance and store the specified message and source data. 
+     * 
+     * @param message The message for the exception. 
+     * @param source  The original exception that lead to this exception. This
+     *                can be <code>null</code>.
+     */
+    public SWORDException(String message, Exception source)
+    {  
+       super(message, source); 
+    }
+    
+    /**
+     * Create a new instance and store the specified message and source data. 
+     * 
+     * @param message The message for the exception. 
+     * @param source  The original exception that lead to this exception. This
+     *                can be <code>null</code>.
+     * @param errorCode The error code to sed back with the request
+     */
+    public SWORDException(String message, Exception source, String errorCode)
+    {  
+       super(message, source); 
+       this.errorCode = errorCode;
+    }
+    
+    /**
+     * Create a new instance and store the specified message. 
+     * 
+     * @param message The message for the exception. 
+     */
+    public SWORDException(String message)
+    {
+       super(message);
+    }
+    
+    /**
+     * Get the error code
+     * 
+     * @return The error code
+     */
+    public String getErrorCode() {
+    	return errorCode;
+    }
 }
