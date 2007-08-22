aec386cdbbb2852bf021d563029133dde5421179
stuart.lewis <stuart.lewis@2b3ed498-6d30-0410-9511-f9d6487c6e61>
2007-08-22 19:54:09 +0000

    First commit

---
+++ branches/sword/dspace-sword/src/main/java/org/purl/sword/base/InfoLoggerLevel.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.purl.sword.base;
+
+/**
+ * This enumeration determines the logging level to be used for the InfoLogger class. 
+ * 
+ * @author Neil Taylor (nst@aber.ac.uk)
+ */
+public enum InfoLoggerLevel
+{
+   ERROR, 
+   ERROR_WARNING, 
+   ERROR_WARNING_INFO
+}
